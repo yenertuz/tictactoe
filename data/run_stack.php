5dca0d26d099eef06b6c900636533315258737c1
Yener Tuz <ytuz@e1z2r3p18.42.us.org>
2018-11-15 19:38:54 -0800

    test

--- data/run_stack.php
+++ data/run_stack.php
@@ -1,23 +1,37 @@
 <?php
 
-function	main()
+function	get_stack_string()
 {
-	if (file_exists("stack.tsv") == 0)
-	{
+	if (file_exists("stack") == 0)
 		exit(-1);
-	}
-	$stack_string = trim(file_get_contents("stack.tsv"));
+	$stack_string = file_get_contents("stack");
+	$stack_string = trim($stack_string);
 	if (strlen($stack_string) == 0)
-	{
-		exit(0);
-	}
-	$stack_array = explode("\n", $stack_string);
-	$focus = $stack_array[0];
-	$results = get_results($focus);
-	rewrite_stack($stack_array, $focus, $results);
+		exit(-1);
+	return ($stack_string);
 }
 
+function	process_focus($focus)
+{
+	$move_count = substr_count($focus, ".");
+	echo "{$move_count} POSSIBLE MOVES";
+}
 
+function	rewrite_stack($focus)
+{
+	;
+}
+
+function	main()
+{
+	$stack_string = get_stack_string();
+	$stack_array = explode("\n", $stack_string);
+	$focus = $stack_array[0];
+	process_focus($focus);
+	rewrite_stack($focus);
+}
+
+main();
 
 
 ?>
